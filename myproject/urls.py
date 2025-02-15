ee566792eef40c7651a55e61a714409ba867b137
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 07:02:31 +0900

    update: Swagger UI 테스트 설정

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version="v1",
+        description="API Documentation",
+    ),
+    public=True,
+)
 
 urlpatterns = [
-    path('api/accounts/', include('accounts.urls')), # accounts 앱의 URL을 포함
+    path('api/accounts/', include('accounts.urls')), # accounts 앱 URLconf 등록    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
