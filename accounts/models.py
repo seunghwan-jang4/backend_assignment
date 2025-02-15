a008fda141504b6c266314eb98153dd4a86e60bc
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 05:59:34 +0900

    updata: CustomUser추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-class Account(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.username
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=30, unique=True)  # 추가 필드
