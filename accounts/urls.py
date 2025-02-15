5328e79f0e4a567820d175d882a7a099341f6047
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 06:09:13 +0900

    update: 주석추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SignupView, LoginView
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignupView.as_view(), name='signup'),  # 회원가입 엔드포인트
+    path('login/', LoginView.as_view(), name='login'),  # 로그인 엔드포인트
 ]
