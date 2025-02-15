6ea9493ba558d0011f6a7a34ac294e5d7093f62d
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 06:43:48 +0900

    update: JWT 토큰 갱신 엔드포인트 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import SignupView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),  # 회원가입 엔드포인트
     path('login/', LoginView.as_view(), name='login'),  # 로그인 엔드포인트
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # JWT 토큰 갱신 엔드포인트
 ]
