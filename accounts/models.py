8fd1a033dbfc6905b317958cd5e6b00ba62b490e
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 06:00:27 +0900

    update: 주석추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import AbstractUser  # Django의 기본 사용자 모델을 확장하기 위해 가져옴
+from django.db import models  
 
-class CustomUser(AbstractUser):
-    nickname = models.CharField(max_length=30, unique=True)  # 추가 필드
+class CustomUser(AbstractUser):  # AbstractUser를 상속받아 사용자 모델을 커스터마이징
+    nickname = models.CharField(max_length=30, unique=True)  # 사용자에게 고유한 닉네임을 부여하는 추가 필드
