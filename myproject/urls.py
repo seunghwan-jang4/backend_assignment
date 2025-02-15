1df27f4b9a3d9603b38c08eab8ebabc58c94c4a6
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 07:33:43 +0900

    update: 기본 로그인 url 추가

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.auth import views as auth_views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -30,6 +31,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api/accounts/', include('accounts.urls')), # accounts 앱 URLconf 등록    
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'), # Swagger UI 엔드포인트
+    path('api/accounts/', include('accounts.urls')),  # accounts 앱 URLconf 등록    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),  # Swagger UI 엔드포인트
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),  # Django 기본 로그인 URL 추가
 ]
