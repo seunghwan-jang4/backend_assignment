0d2221113204429b81cd518e9bbe7cb4200afd44
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 20:29:20 +0900

    update: 앱이름 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
+from . import views
 from django.urls import path
-from .views import SignupView, LoginView, LogoutView, UserProfileView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenBlacklistView
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),  # 회원가입 엔드포인트
-    path('login/', LoginView.as_view(), name='login'),  # 로그인 엔드포인트
-    path('logout/', LogoutView.as_view(), name='logout'),  # 로그아웃 엔드포인트 추가
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # JWT 토큰 갱신 엔드포인트
-    path('profile/', UserProfileView.as_view(), name='user-profile'),  # 프로필 조회 API 추가
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
