f1994e85fa17a077731f2534fc5dabd7dc450094
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 21:01:06 +0900

    update: 사용자매니저, 모델 정의 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,29 @@
-from django.contrib.auth.models import AbstractUser  # Django의 기본 사용자 모델을 확장하기 위해 가져옴
-from django.db import models  
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-class CustomUser(AbstractUser):  # AbstractUser를 상속받아 사용자 모델을 커스터마이징
-    nickname = models.CharField(max_length=30, unique=True)  # 사용자에게 고유한 닉네임을 부여하는 추가 필드
+# 사용자 매니저 정의
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('사용자명은 필수입니다')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, password, **extra_fields)
+
+# 사용자 모델 정의
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=30, unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['nickname']  # 필수 필드 추가
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username  # 사용자명 반환
