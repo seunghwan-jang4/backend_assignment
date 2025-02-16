e747c892ecf14986097272e89eccabb59537ebfe
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-16 21:08:32 +0900

    update: urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from . import views
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenBlacklistView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('signup/', views.signup, name='signup'),  # 회원가입 URL
+    path('login/', views.login, name='login'),  # 로그인 URL
+    path('logout/', views.LogoutView.as_view(), name='logout'),  # 로그아웃 URL
+    path('profile/', views.UserProfileView.as_view(), name='profile'),  # 프로필 조회 URL
+    path('token/create/', views.create_token, name='create_token'),  # 토큰 생성 URL
+    path('token/verify/', views.verify_token, name='verify_token'),  # 토큰 검증 URL
 ]
